0d60d7c9675639652611b4a35ad1a67ac4c56e3c
Tariq Almazyad <34104180+TariqAlmazyad@users.noreply.github.com>
2022-09-01 20:50:46 +0300

    Initial Commit

---
+++ SwiftUIDragDropDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDragDropDemo
+//
+//  Created by Tariq Almazyad on 01/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDragDropDemo/SwiftUIDragDropDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDragDropDemoApp.swift
+//  SwiftUIDragDropDemo
+//
+//  Created by Tariq Almazyad on 01/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDragDropDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
